2df734fac9441d76e8d5fe855f3cb1a67d3ebfd6
mostafiz9900 <mostafiz9900@gmail.com>
2019-01-13 13:03:42 +0600

    book angular 6

---
+++ chapter_6/src/java/com/menu/MegaMenuView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.menu;
+
+/**
+ *
+ * @author User
+ */
+public class MegaMenuView {
+    
+}
---
+++ chapter_6/src/java/com/menu/MenuView.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.menu;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.model.menu.DefaultMenuItem;
+import org.primefaces.model.menu.DefaultMenuModel;
+import org.primefaces.model.menu.DefaultSubMenu;
+import org.primefaces.model.menu.MenuModel;
+
+/**
+ *
+ * @author User
+ */
+@ManagedBean
+@ViewScoped
+public class MenuView {
+     private MenuModel model;
+ 
+    @PostConstruct
+    public void init() {
+        model = new DefaultMenuModel();
+ 
+        //First submenu
+        DefaultSubMenu firstSubmenu = new DefaultSubMenu("Dynamic Submenu");
+ 
+        DefaultMenuItem item = new DefaultMenuItem("External");
+        item.setUrl("http://www.primefaces.org");
+        item.setIcon("pi pi-home");
+        firstSubmenu.addElement(item);
+ 
+        model.addElement(firstSubmenu);
+ 
+        //Second submenu
+        DefaultSubMenu secondSubmenu = new DefaultSubMenu("Dynamic Actions");
+ 
+        item = new DefaultMenuItem("Save");
+        item.setIcon("pi pi-save");
+        item.setCommand("#{menuView.save}");
+        item.setUpdate("messages");
+        secondSubmenu.addElement(item);
+ 
+        item = new DefaultMenuItem("Delete");
+        item.setIcon("pi pi-times");
+        item.setCommand("#{menuView.delete}");
+        item.setAjax(false);
+        secondSubmenu.addElement(item);
+ 
+        item = new DefaultMenuItem("Redirect");
+        item.setIcon("pi pi-search");
+        item.setCommand("#{menuView.redirect}");
+        secondSubmenu.addElement(item);
+ 
+        model.addElement(secondSubmenu);
+    }
+ 
+    public MenuModel getModel() {
+        return model;
+    }
+ 
+    public void save() {
+        addMessage("Success", "Data saved");
+    }
+ 
+    public void update() {
+        addMessage("Success", "Data updated");
+    }
+ 
+    public void delete() {
+        addMessage("Success", "Data deleted");
+    }
+ 
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}

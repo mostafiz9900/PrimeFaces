e8c735d19db9f9477895145b0f5353762c9b0377
mostafiz9900 <mostafiz9900@gmail.com>
2019-01-13 13:08:53 +0600

    book angular 6

--- chapter_6/src/java/com/menu/MegaMenuView.java
+++ chapter_6/src/java/com/menu/MegaMenuView.java
@@ -5,10 +5,21 @@
  */
 package com.menu;
 
+import javax.faces.bean.ManagedBean;
+
 /**
  *
  * @author User
  */
+@ManagedBean
 public class MegaMenuView {
-    
+       private String orientation = "horizontal";
+ 
+    public String getOrientation() {
+        return orientation;
+    }
+ 
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
 }
